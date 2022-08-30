79fb828aac3f908ca3a8c5a26e105e3b3e217e5a
Egor Kirdyapin <haruhoshimain@gmail.com>
2022-08-30 17:57:39 +0300

    scripts changes

---
+++ script/menu.js
@@ -0,0 +1,7 @@
+const humburger__btn = document.querySelector('#humburger__btn');
+        const menu__list = document.querySelector('#menu__list');
+
+        [humburger__btn, menu__list].forEach((elem)=>{elem.addEventListener("click", function(){
+            document.body.classList.toggle('lock');
+        }); 
+    });
