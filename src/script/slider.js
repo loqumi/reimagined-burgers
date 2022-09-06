db505f162f6c65351b25563e49674031f1ebd128
Egor Kirdyapin <haruhoshimain@gmail.com>
2022-09-06 17:36:45 +0300

    code fix(code fix`a)

--- src/script/slider.js
+++ src/script/slider.js
@@ -6,28 +6,28 @@ const max_count = items.querySelectorAll("li").length;
 const consist__btn = document.querySelectorAll(".consist__btn");
 
   right.addEventListener("click", function() {
+    
+    items.style.transform = `translateX(${-100 * click_count}%)`;
 
     if ((click_count) < max_count) {
     click_count = ++click_count;
-    items.style.transform = `translateX(${-100 * click_count}%)`;
     }
 
     if ((click_count) == max_count) {
     click_count = 0;
-    items.style.transform = `translateX(${-100 * click_count}%)`;
     }
   });
 
   left.addEventListener("click", function() {
 
+    items.style.transform = `translateX(${-100 * click_count}%)`;
+
     if ((click_count) > -1) {
     click_count = --click_count;
-    items.style.transform = `translateX(${-100 * click_count}%)`;
     }
 
     if ((click_count) == -1) {
     click_count = max_count -1;
-    items.style.transform = `translateX(${-100 * click_count}%)`;
     }
   });
 
