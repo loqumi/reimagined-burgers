b047e6d69bc2e4f1aed3577a2debf675544a3d85
Egor Kirdyapin <haruhoshimain@gmail.com>
2022-09-06 14:31:39 +0300

    phone event(js)

--- src/script/order.js
+++ src/script/order.js
@@ -2,15 +2,38 @@ const orderform = document.querySelector('.order__form');
 const btn = document.querySelector('.order__btn');
 const answ_window = document.querySelector('.response__order');
 const response_btn = document.querySelector('.response_btn');
+const phone = document.querySelector('#phone');
 const response_answer = document.querySelector('.response-answer');
+
+    phone.addEventListener('keydown', function(event){
+        let isDigit = false;
+        let isDash = false;
+        let isControl = false;
+
+        if (event.key >=0 || event.key <=9) {
+            isDigit = true;
+        }
+        
+        if (event.key == '-'){
+            isDash = true;
+        }
+
+        if (event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace'){
+            isControl = true;
+        }
+
+        if (!isDigit && !isDash && !isControl){
+            event.preventDefault();
+        }
+    })
 
     btn.addEventListener('click', function(event) {
         event.preventDefault();
         
         if (validateForm(orderform)) {
             const data = {
-                firstName:orderform.elements.firstName.value,
-                telephone:orderform.elements.telephone.value,
+                firstName:orderform.elements.name.value,
+                telephone:orderform.elements.phone.value,
                 comment:orderform.elements.comment.value
             };
 
@@ -26,7 +49,7 @@ const response_answer = document.querySelector('.response-answer');
     });
 
     function validateForm(form){
-        return [form.elements.firstName, form.elements.telephone, form.elements.comment].every(elem => (validateField(elem)))
+        return [form.elements.name, form.elements.phone, form.elements.comment].every(elem => (validateField(elem)))
     }
 
     const VALID_MESSAGE = {
