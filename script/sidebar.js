ecaa104656f2162d443e1ae8e32dfb426a42bcb7
Egor Kirdyapin <haruhoshimain@gmail.com>
2022-09-19 14:27:15 +0300

    mobile js swipe

--- script/sidebar.js
+++
@@ -1,6 +0,0 @@
-const dot_items = document.querySelectorAll('.dot_item');
-
-dot_items.forEach((elem) => {
-    elem.addEventListener("click", () => {
-    });
-});
--- script/utils.js
+++ script/utils.js
@@ -1,10 +1,5 @@
-const wrapper = document.querySelector('.wrapper-body');
 const dot_items = document.querySelectorAll('.dot_item');
 
-export function wrapper_translate(num){
-    wrapper.style.transform = `translateY(${-100 * num}vh)`;
-    set_active(num)
-}
 export function set_active(num){
     dot_items.forEach((elem) => elem.classList.remove('active'))
         
